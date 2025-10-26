e791accce1e500b8a6a10866721eb3afec3da855
MaartenMaes <r0940730@student.thomasmore.be>
2025-10-26 21:01:04 +0100

    Almost done

--- apache/src/index.php
+++ apache/src/index.php
@@ -1,27 +1,25 @@
 <?php
-$servername = "contsql-m1-10"; // Use your container name
-$username = "root";             // Default MySQL user
-$password = "rootpassword";     // Set a strong password
-$dbname = "milestone1";
+$servername = "db"; // Name of the container
+$username = "root";             // The default MYSQL user
+$password = "rootpassword";     // Setting a MYSQL password
+$dbname = "milestone1"; // Name of the database
 
-// Connect to DB
+// Use these variables to connect to the database
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($conn->connect_error) { // Check the connection to the database
+    die("Connection failed: " . $conn->connect_error); // If the connection fails, stop executing and leave an error message behind
 }
 
-// Fetch name
-$sql = "SELECT fullname FROM userinfo LIMIT 1";
-$result = $conn->query($sql);
+$sql = "SELECT fullname FROM userinfo LIMIT 1"; // Fetch the name from the userinfo table with the limit of 1 record
+$result = $conn->query($sql); // Pass the connection query to a variable called result
 
-if ($result->num_rows > 0) {
+if ($result->num_rows > 0) { // If result turned back at least 1 row get the fullname record from the table in the database and put it into a string.
     $row = $result->fetch_assoc();
     echo "<h1>" . $row['fullname'] . " has reached Milestone 1!!</h1>";
 } else {
-    echo "No name found in DB.";
+    echo "No name found in DB."; // If the result is empty leave an info message behind
 }
 
-$conn->close();
+$conn->close(); // Close the connection to the database
 ?>
--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE milestone1;
-USE milestone1;
+CREATE DATABASE IF NOT EXISTS milestone1; --Create a database if it doesn't exist yet called milestone1
+USE milestone1; --Use the milestone1 database
 
-CREATE TABLE userinfo (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    fullname VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS userinfo ( --Create a table in the milestone1 database if this doesn't exist yet
+    id INT AUTO_INCREMENT PRIMARY KEY, --Create a primary key called id which is an integer that will automatically increment (+1)
+    fullname VARCHAR(255) NOT NULL --Create fullname column which cannot be NULL and has the maximum amount of 255 characters
 );
 
-INSERT INTO userinfo (fullname) VALUES ('Maarten Maes');
+INSERT INTO userinfo (fullname) VALUES ('Maarten Maes'); --Insert the value Maarten Maes as the fullname column into the userinfo table
